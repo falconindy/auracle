f4b5f7c0c99de576d5dd8364e810d4d77c476fe4
Dave Reisner <dreisner@archlinux.org>
2019-02-24 15:52:38 -0500

    Avoid use of deprecated/removed functions
    - std::ptr_fun was deprecated in c++11, removed in c++17
    - std::not1 was deprecated in c++11, removed in c++20
    
    Pointed out in #41 as part of supporting libc++.

--- src/auracle/pacman.cc
+++ src/auracle/pacman.cc
@@ -24,18 +24,15 @@ struct default_delete<glob_t> {
 
 namespace {
 
+bool notspace(char c) { return !std::isspace(c); }
+
 std::string* ltrim(std::string* s) {
-  s->erase(s->begin(),
-           std::find_if(s->begin(), s->end(),
-                        std::not1(std::ptr_fun<int, int>(std::isspace))));
+  s->erase(s->begin(), std::find_if(s->begin(), s->end(), &notspace));
   return s;
 }
 
 std::string* rtrim(std::string* s) {
-  s->erase(std::find_if(s->rbegin(), s->rend(),
-                        std::not1(std::ptr_fun<int, int>(std::isspace)))
-               .base(),
-           s->end());
+  s->erase(std::find_if(s->rbegin(), s->rend(), &notspace).base(), s->end());
   return s;
 }
 
