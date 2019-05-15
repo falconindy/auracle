falconindy/auracle

commit 1e0aeb9a16fc9d84fbccd29ba140e82626a1e031

Don't reject "too short" query arguments with --literal

tests/fakeaur/server.py
@@ -73,6 +73,10 @@ class FakeAurHandler(http.server.BaseHTTPRequestHandler):
 
 
     def handle_rpc_search(self, arg, by):
+        if len(arg) < 2:
+            return self.respond(response=self.make_json_reply(
+                'error', error='Query arg too small.'))
+
         reply = self.lookup_response(
                 'search', '{}|{}'.format(by, arg) if by else arg)
 

src/auracle/auracle.cc
@@ -119,12 +119,8 @@ std::vector<std::string> NotFoundPackages(
   return missing;
 }
 
-std::string GetSearchFragment(const std::string& s, bool allow_regex) {
+std::string GetSearchFragment(const std::string& s) {
   static constexpr char kRegexChars[] = "^.+*?$[](){}|\\";
-
-  if (!allow_regex) {
-    return s;
-  }
 
   int span = 0;
   const char* argstr;
@@ -343,11 +339,14 @@ int Auracle::Search(const std::vector<std::string>& args,
 
   std::vector<aur::Package> packages;
   for (const auto& arg : args) {
-    auto frag = GetSearchFragment(arg, options.allow_regex);
-    if (frag.empty()) {
-      std::cerr << "error: search string '" << arg
-                << "' insufficient for searching by regular expression.\n";
-      return -EINVAL;
+    std::string frag = arg;
+    if (options.allow_regex) {
+      frag = GetSearchFragment(arg);
+      if (frag.empty() && options.allow_regex) {
+        std::cerr << "error: search string '" << arg
+                  << "' insufficient for searching by regular expression.\n";
+        return -EINVAL;
+      }
     }
 
     aur_.QueueRpcRequest(aur::SearchRequest(options.search_by, frag),

tests/regex_search.py
@@ -32,14 +32,5 @@ class TestRegexSearch(auracle_test.TestCase):
         ])
 
 
-    def testLiteralSearch(self):
-        p = self.Auracle(['search', '--literal', '^aurac.+'])
-        self.assertEqual(p.returncode, 0)
-
-        self.assertListEqual(self.request_uris, [
-            '/rpc?v=5&type=search&by=name-desc&arg=%5Eaurac.%2B',
-        ])
-
-
 if __name__ == '__main__':
     auracle_test.main()

tests/search.py
@@ -66,5 +66,15 @@ class TestSearch(auracle_test.TestCase):
         ])
 
 
+
+    def testLiteralSearchWithShortTerm(self):
+        p = self.Auracle(['search', '--literal', 'a'])
+        self.assertEqual(p.returncode, 1)
+
+        self.assertListEqual(self.request_uris, [
+            '/rpc?v=5&type=search&by=name-desc&arg=a',
+        ])
+
+
 if __name__ == '__main__':
     auracle_test.main()

commit b7850fa38086e8b239aea0b967d4c22dcd6d46ab

Fix exception in reply generation
This was introduced in 1e0aeb9a16fc9d84fbccd29ba140e82626a1e031 but
isn't caught by the test framework, because:

- the base socketserver catches the exception and trudges on
- even if the above, we never consider the exit status of the server
  process

We'll deal with the above in a future commit.

tests/fakeaur/server.py
@@ -38,7 +38,7 @@ class FakeAurHandler(http.server.BaseHTTPRequestHandler):
         return l[-1] if l else None
 
 
-    def make_json_reply(self, querytype, results, error=None):
+    def make_json_reply(self, querytype, results=[], error=None):
         return json.dumps({
             'version': AUR_SERVER_VERSION,
             'type': querytype,
@@ -46,6 +46,14 @@ class FakeAurHandler(http.server.BaseHTTPRequestHandler):
             'results': results,
             'error': error,
         }).encode()
+
+
+    def make_package_reply(self, querytype, results):
+        return self.make_json_reply(querytype, results)
+
+
+    def make_error_reply(self, error_message):
+        return self.make_json_reply('error', [], error_message)
 
 
     def make_pkgbuild(self, pkgname):
@@ -58,7 +66,7 @@ class FakeAurHandler(http.server.BaseHTTPRequestHandler):
             with open(path) as f:
                 return f.read().strip().encode()
         except FileNotFoundError:
-            return self.make_json_reply(querytype, [])
+            return self.make_package_reply(querytype, [])
 
 
     def handle_rpc_info(self, args):
@@ -69,13 +77,14 @@ class FakeAurHandler(http.server.BaseHTTPRequestHandler):
             # extract the results from each DB file
             results.extend(json.loads(reply)['results'])
 
-        return self.respond(response=self.make_json_reply('multiinfo', results))
+        return self.respond(response=self.make_package_reply(
+            'multiinfo', results))
 
 
     def handle_rpc_search(self, arg, by):
         if len(arg) < 2:
-            return self.respond(response=self.make_json_reply(
-                'error', error='Query arg too small.'))
+            return self.respond(response=self.make_error_reply(
+                'Query arg too small.'))
 
         reply = self.lookup_response(
                 'search', '{}|{}'.format(by, arg) if by else arg)
@@ -93,8 +102,8 @@ class FakeAurHandler(http.server.BaseHTTPRequestHandler):
             self.handle_rpc_search(self.last_of(queryparams.get('arg')),
                                    self.last_of(queryparams.get('by')))
         else:
-            return self.respond(response=self.make_json_reply(
-                'error', error='Incorrect request type specified.'))
+            return self.respond(response=self.make_json_error_reply(
+                'Incorrect request type specified.'))
 
 
     def handle_download(self, url):