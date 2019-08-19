f6e0ca1ad0b749d1032e533859dbb12a7cda6580
Dave Reisner <dreisner@archlinux.org>
2019-08-19 07:54:00 -0400

    Simplify time conversion, remove template param
    We can extend this in the future if we need something more generic to
    handle conversions to things other than unix microseconds.

--- src/aur/aur.cc
+++ src/aur/aur.cc
@@ -104,9 +104,10 @@ std::string_view GetEnv(const char* name) {
   return std::string_view(value ? value : "");
 }
 
-template <typename TimeRes, typename ClockType>
-auto TimepointTo(std::chrono::time_point<ClockType> tp) {
-  return std::chrono::time_point_cast<TimeRes>(tp).time_since_epoch().count();
+template <typename ClockType>
+auto ToUnixMicros(std::chrono::time_point<ClockType> tp) {
+  auto micros = std::chrono::time_point_cast<std::chrono::microseconds>(tp);
+  return micros.time_since_epoch().count();
 }
 
 bool ConsumePrefix(std::string_view* view, std::string_view prefix) {
@@ -376,8 +377,8 @@ int AurImpl::DispatchTimerCallback(long timeout_ms) {
     return 0;
   }
 
-  auto usec = TimepointTo<std::chrono::microseconds>(
-      std::chrono::steady_clock::now() + std::chrono::milliseconds(timeout_ms));
+  uint64_t usec = ToUnixMicros(std::chrono::steady_clock::now() +
+                               std::chrono::milliseconds(timeout_ms));
 
   if (timer_ != nullptr) {
     if (sd_event_source_set_time(timer_, usec) < 0) {
