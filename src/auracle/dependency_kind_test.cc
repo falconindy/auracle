047e031e94b34bf15f6834309b6ad5d1d8c2a08a
Dave Reisner <dreisner@archlinux.org>
2020-05-23 06:55:57 -0400

    Make DependencyKind tests more hermetic
    Create a new std::set<DK> for each operation such that one test failing
    early doesn't cause them all to potentially break (given that we're
    using EXPECT and not ASSERT). Also add a PrintTo overload so that
    failures are more easily understood:
    
    Before:
    
      Expected: has 2 elements and there exists some permutation of elements such that:
       - element #0 is equal to 4-byte object <00-00 00-00>, and
       - element #1 is equal to 4-byte object <01-00 00-00>
        Actual: { 4-byte object <00-00 00-00> }, which has 1 element
    
    After:
    
      Expected: has 3 elements and there exists some permutation of elements such that:
       - element #0 is equal to DependencyKind::Depend, and
       - element #1 is equal to DependencyKind::MakeDepend, and
       - element #2 is equal to DependencyKind::CheckDepend
        Actual: { DependencyKind::MakeDepend, DependencyKind::CheckDepend }, which has 2 elements

--- src/auracle/dependency_kind_test.cc
+++ src/auracle/dependency_kind_test.cc
@@ -2,45 +2,80 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+namespace auracle {
+
+void PrintTo(DependencyKind kind, std::ostream* os) {
+  switch (kind) {
+    case DependencyKind::Depend:
+      *os << "Depend";
+      break;
+    case DependencyKind::MakeDepend:
+      *os << "MakeDepend";
+      break;
+    case DependencyKind::CheckDepend:
+      *os << "CheckDepend";
+      break;
+  }
+}
+
+}  // namespace auracle
+
 namespace {
 
 TEST(AuracleTest, ParseDependencyKinds) {
   using DK = auracle::DependencyKind;
 
-  std::set<DK> kinds;
+  {
+    // Overwrite
+    std::set<DK> kinds;
+    EXPECT_TRUE(ParseDependencyKinds("depends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+    EXPECT_TRUE(
+        ParseDependencyKinds("depends,checkdepends,makedepends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(
+                           DK::Depend, DK::CheckDepend, DK::MakeDepend));
+  }
 
-  // Overwrite
-  EXPECT_TRUE(ParseDependencyKinds("depends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
-  EXPECT_TRUE(ParseDependencyKinds("depends,checkdepends,makedepends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend, DK::CheckDepend,
-                                                   DK::MakeDepend));
+  {
+    // Remove
+    std::set<DK> kinds = {DK::Depend, DK::MakeDepend, DK::CheckDepend};
+    EXPECT_TRUE(ParseDependencyKinds("^checkdepends", &kinds));
+    EXPECT_THAT(kinds,
+                testing::UnorderedElementsAre(DK::Depend, DK::MakeDepend));
+    EXPECT_TRUE(ParseDependencyKinds("!depends,makedepends", &kinds));
+    EXPECT_THAT(kinds, testing::IsEmpty());
+  }
 
-  // Remove
-  EXPECT_TRUE(ParseDependencyKinds("^checkdepends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend, DK::MakeDepend));
-  EXPECT_TRUE(ParseDependencyKinds("!depends,makedepends", &kinds));
-  EXPECT_THAT(kinds, testing::IsEmpty());
+  {
+    // Append
+    std::set<DK> kinds;
+    EXPECT_TRUE(ParseDependencyKinds("+depends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+    EXPECT_TRUE(ParseDependencyKinds("+makedepends,checkdepends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend, DK::MakeDepend,
+                                                     DK::CheckDepend));
+  }
 
-  // Append
-  EXPECT_TRUE(ParseDependencyKinds("+depends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
-  EXPECT_TRUE(ParseDependencyKinds("+makedepends,checkdepends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend, DK::MakeDepend,
-                                                   DK::CheckDepend));
+  {
+    // Bad spelling
+    std::set<DK> kinds = {DK::Depend};
+    EXPECT_FALSE(ParseDependencyKinds("derpends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+  }
 
-  // Bad spelling
-  kinds = {DK::Depend};
-  EXPECT_FALSE(ParseDependencyKinds("derpends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+  {
+    // Negation in the middle of a string isn't allowed
+    std::set<DK> kinds = {DK::Depend};
+    EXPECT_FALSE(ParseDependencyKinds("depends,!makedepends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+  }
 
-  // Negation in the middle of a string isn't allowed
-  EXPECT_FALSE(ParseDependencyKinds("depends,!makedepends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
-
-  // Bad second element still leaves out param untouched.
-  EXPECT_FALSE(ParseDependencyKinds("depends,!makdepends", &kinds));
-  EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+  {
+    // Bad second element still leaves out param untouched.
+    std::set<DK> kinds = {DK::Depend};
+    EXPECT_FALSE(ParseDependencyKinds("depends,!makdepends", &kinds));
+    EXPECT_THAT(kinds, testing::UnorderedElementsAre(DK::Depend));
+  }
 }
 
 }  // namespace
